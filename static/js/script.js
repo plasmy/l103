2badc5cee1d4b6d6c41016206da5f1ab470b34b5
plasmy <mariostar1@gmail.com>
2018-08-05 20:18:46 -0400

    Pagination for HTML Table.

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,6 @@
+var pageCount = 2;
+var dataResponse;
+
 $(document).ready(function(){
     //Code for the search button
     $('[name=search]').click(function(){
@@ -11,26 +14,50 @@ $(document).ready(function(){
             urlSearch += '/' + month;
 
         $.get(urlSearch, function(response){
+            dataResponse = response;
+            pageCount = response.length / 10; //Amount of pages in pagination. Each page has 10 rows.
+            paginate();
             $('[name=download]').show(); //Show download button
             $('[name=tableResult]').show(); //Show table with data
-            $('[name=tableResult] tbody tr').remove(); //Remove existing data in the table
 
-            //Create table columns with data and add to row, then to table
-            $.each(response, function(key, value){
-                var tr = $("<tr>");
-                var td =
-                '<td>' + value.NU_ENTIDAD + '</td>'+
-                '<td>' + value.RG_TRANS + '</td>' +
-                '<td>' + value.RG_COL + '</td>' +
-                '<td>' + value.RG_ROW + '</td>' +
-                '<td>' + value.RG_VALUE + '</td>' +
-                '<td>' + value.CYYYYMM + '</td>' +
-                '<td>' + value.TRANS_FILETYPE + '</td>'
+            //Create first page of table.
+            loadPage(0);
 
-                tr.append($(td));
-                $('[name=tableResult] tbody').append(tr);
-            });
             $('body').removeClass("loading");
         });
     });
+
+    //Create Pagination of table
+    function paginate(){
+        console.log('Hey')
+        for(var i = 0; i < pageCount; i++){
+            var page = $("<li><a href='#' value='" + (i * 10) + "'>" + (i + 1) + "</a></li>");
+            page.click(function(e){
+                var startingIndex = ($(e.target).html() - 1) * 10;
+                loadPage(startingIndex);
+            });
+            $('.pagination').append(page);
+        }
+    }
+
+    //Load clicked page's data
+    function loadPage(page){
+        $('[name=tableResult] tbody tr').remove(); //Remove existing data in the table
+        //Create table columns with data and add to row, then to table
+        for(var i = page; i < page + 10; i++){
+            var tr = $("<tr>");
+            var td =
+            '<td>' + dataResponse[i].NU_ENTIDAD + '</td>'+
+            '<td>' + dataResponse[i].RG_TRANS + '</td>' +
+            '<td>' + dataResponse[i].RG_COL + '</td>' +
+            '<td>' + dataResponse[i].RG_ROW + '</td>' +
+            '<td>' + dataResponse[i].RG_VALUE + '</td>' +
+            '<td>' + dataResponse[i].CYYYYMM + '</td>' +
+            '<td>' + dataResponse[i].TRANS_FILETYPE + '</td>'
+
+            tr.append($(td));
+            $('[name=tableResult] tbody').append(tr);
+            if(i + 1 == dataResponse.length) break; //Exit if there is no more data
+        }
+    }
 });
